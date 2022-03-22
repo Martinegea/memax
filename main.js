1b26aae89c04089c7c7cffa40cecd64c27a69393
Martin Egea <egea.martin@hotmail.com>
2022-03-22 10:57:22 -0300

    arrays

--- main.js
+++ main.js
@@ -15,3 +15,48 @@ function convertir() {
                 alert("Tiene que completar los datos");
         }
 }
+
+const memax1 = "mate"
+const memax2 = "soporte"
+const memax3 = "lapicero"
+
+console.log (memax1);
+
+const memax = ["mate", "soporte", "lapicero"];
+console.log (memax);
+console.log (memax[0]);
+
+console.log(memax.length);
+console.log(memax[memax.length - 1]);
+
+const listaCompras = [];
+
+let cantidad = 5;
+
+do {
+        let compra = prompt("Cargar productos");
+        console.log(compra);
+        listaCompras.push(compra);
+
+} while(listaCompras.length<=cantidad)
+
+carrito = []
+
+const p1 = {
+        nombre: "Mate Darth Vader",
+        precio: 1500,
+}
+
+const p2 = {
+        nombre: "Caldero Harry",
+        precio: 1100
+}
+
+carrito.push(p1);
+carrito.push(p2);
+
+
+for(let item of carrito){
+        console.log(item);
+        console.log(item.precio);
+}
