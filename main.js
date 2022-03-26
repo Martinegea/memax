c970db8a2ac9aaa93ab3721bb34e1045aec9537d
Martin Egea <egea.martin@hotmail.com>
2022-03-25 23:41:46 -0300

    primera entrega

--- main.js
+++ main.js
@@ -1,62 +1,29 @@
-function convertir() {
-        var valore = parseInt(document.getElementById("valor").value);
-        var resultado = 0;
-        var dolar = 110;
-        var euro = 121;
-        if (document.getElementById("uno").checked){
-                resultado = valore / dolar;
-                alert("El cambio de pesos a dolar es: $" + resultado);
-        }
-        else if (document.getElementById("dos").checked){
-                resultado = valore / euro;
-                alert("El cambio de pesos a euro es: $" + resultado);
-        }
-        else{
-                alert("Tiene que completar los datos");
-        }
+let nombre = prompt("Dime tu nombre");
+
+alert("Hola " + nombre + " bienvenido a memax 3d");
+
+function saludar(){
+        respuesta = prompt(nombre + " ¿Como fue tu día?");
+        if (respuesta == "bien") {
+                 alert("Me alegro")
+        } else {
+                 alert("Vendran días mejores")
 }
 
-const memax1 = "mate"
-const memax2 = "soporte"
-const memax3 = "lapicero"
-
-console.log (memax1);
-
-const memax = ["mate", "soporte", "lapicero"];
-console.log (memax);
-console.log (memax[0]);
-
-console.log(memax.length);
-console.log(memax[memax.length - 1]);
-
-const listaCompras = [];
-
-let cantidad = 5;
-
-do {
-        let compra = prompt("Cargar productos");
-        console.log(compra);
-        listaCompras.push(compra);
-
-} while(listaCompras.length<=cantidad)
-
-carrito = []
-
-const p1 = {
-        nombre: "Mate Darth Vader",
-        precio: 1500,
 }
 
-const p2 = {
-        nombre: "Caldero Harry",
-        precio: 1100
+saludar()
+
+
+const objeto1 = { id: 1, producto: "Llavero" };
+const array = [objeto1, { id: 2, producto: "Sombrero Harry"}];
+array.push({ id: 3, producto: "Soporte celular"});
+
+console.log(array);
+
+let tabla = prompt(nombre + " Ingresa un número y te muestro su tabla de multiplicar");
+
+for (var i = 1; i < 11; i++) {
+        console.log(tabla + " x " + i + " = " + (tabla * i));
 }
 
-carrito.push(p1);
-carrito.push(p2);
-
-
-for(let item of carrito){
-        console.log(item);
-        console.log(item.precio);
-}
