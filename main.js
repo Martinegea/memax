8e63f2c945f3299e8938b601e196563e1f5d8b33
Martin Egea <egea.martin@hotmail.com>
2022-04-02 02:12:38 -0300

    dom mas eventos

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
-let nombre = prompt("Dime tu nombre");
+let nombreProducto=document.getElementById("producto");
 
-alert("Hola " + nombre + " bienvenido a memax 3d");
+let cantidadProducto=document.getElementById("cantidad");
 
-function saludar(){
-        respuesta = prompt(nombre + " ¿Como fue tu día?");
-        if (respuesta == "bien") {
-                 alert("Me alegro")
-        } else {
-                 alert("Vendran días mejores")
+let precioProducto=document.getElementById("precio");
+
+let tarjeta=document.getElementById("tarjetas")
+
+let view=document.getElementById("salida");
+
+function capturar(){
+        view.innerHTML=nombreProducto.value+"<br>"+cantidadProducto.value+"<br>"+precioProducto.value+"<br>"+tarjeta.value;
+
+        let producto=[nombreProducto.value, cantidadProducto.value,precioProducto.value,tarjeta.value];
+
+        console.log(producto);
 }
-
-}
-
-saludar()
-
-
-const objeto1 = { id: 1, producto: "Llavero" };
-const array = [objeto1, { id: 2, producto: "Sombrero Harry"}];
-array.push({ id: 3, producto: "Soporte celular"});
-
-console.log(array);
-
-let tabla = prompt(nombre + " Ingresa un número y te muestro su tabla de multiplicar");
-
-for (var i = 1; i < 11; i++) {
-        console.log(tabla + " x " + i + " = " + (tabla * i));
-}
-
